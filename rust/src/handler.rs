e3402dbf2cbf894c0e7d230f55e337543cb3079f
Joe Watson <jw120@users.noreply.github.com>
2026-04-06 22:52:10 +0800

    Ping Pong passed

--- rust/src/handler.rs
+++ rust/src/handler.rs
@@ -1,70 +1,73 @@
 //! Handler for TCP/IP requests
 
-use anyhow::{Result, bail};
+use anyhow::{Result, anyhow, bail};
+use tracing::{info, trace};
 
-use tokio::io::{AsyncBufRead, AsyncBufReadExt, AsyncReadExt};
+use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt};
 
 
 /// Dummy handler function
-pub async fn handle(stream: impl AsyncBufRead + Unpin) -> Result<()> {
-    let _ = read_command(stream).await?;
-    // let mut line_buffer = String::new();
-
-    // loop {
-    //     line_buffer.clear();
-    //     stream.read_line(&mut line_buffer).await?;
-    //     let trimmed: &str = line_buffer.trim();
-        
-    //     if trimmed.is_empty() {
-    //         break;
-    //     }
-    //     println!("got line: '{trimmed}'");
-    // }
-
+pub async fn handle<S>(mut stream: S) -> Result<()> where S: AsyncBufReadExt + AsyncWriteExt + Unpin,
+{
+    let commands = read_command(&mut stream).await?;
+    for command in &commands {
+        let response = dispatch_command(command)?;
+        stream.write_all(&response).await?;
+        stream.flush().await?;
+        info!("Wrote '{}'",  String::from_utf8_lossy(&response));        
+    }
     Ok(())
 }
 
 /// Read a command from the stream
-async fn read_command(mut stream: impl AsyncBufReadExt + Unpin) -> Result<Vec<Vec<u8>>> {
-    // First line of command is *N\r\m where N is the number of elements
-    let mut line_buffer = String::new();
-    stream.read_line(&mut line_buffer).await?;
-    let Some(without_prefix) = line_buffer.strip_prefix('*') else {
-        bail!("Expected *, got '{}'", line_buffer);
-    };
-    let Some(without_suffix) = without_prefix.strip_suffix("\r\n") else {
-        bail!("Expected line end, got '{}'", line_buffer);
-    };
-    let elements: usize = without_suffix.parse()?;
-    println!("Found {elements} elements");
+async fn read_command<S>(stream: &mut S) -> Result<Vec<Vec<u8>>> where S: AsyncBufReadExt + Unpin,
+{
+    let elements = read_number_line(stream, '*').await?;
     
-    // Read the elements
     let mut commands: Vec<Vec<u8>> = Vec::new();
     for _ in 0..elements {
-        let v = read_bulk_string(&mut stream).await?;
+        let v = read_bulk_string(stream).await?;
         commands.push(v);
     }
 
+    trace!("returning {} commands", commands.len());
     Ok(commands)
     
 }
 
 /// Read a bulk string from the stream
-async fn read_bulk_string(mut stream: impl AsyncBufReadExt + Unpin) -> Result<Vec<u8>> {
-     // First line of command is $N\r\m where N is the number of bytes
+async fn read_bulk_string<S>(stream: &mut S) -> Result<Vec<u8>> where S: AsyncBufReadExt +Unpin {
+
+    let size: usize = read_number_line(stream, '$').await?;
+
+    let mut buf: Vec<u8> = vec![0; size];
+    stream.read_exact(&mut buf).await?;
+    trace!("returning '{:?}'", String::from_utf8_lossy(&buf));
+    Ok(buf)
+}
+
+/// Read a line of format char + unsigned integer + \r\n
+async fn read_number_line<S>(stream: &mut S, c: char) -> Result<usize> where S: AsyncBufReadExt + Unpin {
+
     let mut line_buffer = String::new();
     stream.read_line(&mut line_buffer).await?;
-    let Some(without_prefix) = line_buffer.strip_prefix('$') else {
-        bail!("Expected *, got '{}'", line_buffer);
+    let Some(without_prefix) = line_buffer.strip_prefix(c) else {
+        bail!("Expected {c}, got '{}'", line_buffer);
     };
     let Some(without_suffix) = without_prefix.strip_suffix("\r\n") else {
         bail!("Expected line end, got '{}'", line_buffer);
     };
-    let size: usize = without_suffix.parse()?;
-    println!("Found {size} size");
-
-    let mut buf: Vec<u8> = vec![0; size];
-    stream.read_exact(&mut buf).await?;
-    println!("String {:?}", String::from_utf8_lossy(&buf));
-    Ok(buf)
+    let n: usize = without_suffix.parse()?;
+    trace!("returning {n}");
+    Ok(n)
 }
+
+/// Dispatch the command
+fn dispatch_command(s: &[u8]) -> Result<Vec<u8>> {
+    match s {
+        b"PING" => Ok(Vec::from(b"+PONG\r\n")),
+        _ => Err(anyhow!("Unknown command '{}'",  String::from_utf8_lossy(s))),
+    }
+}
+
+
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,23 +1,43 @@
 //! Http server
 
+use std::io;
+
 use anyhow::Result;
 use tokio::net::TcpListener;
 use tokio::io::BufStream;
+use tracing::info;
+use tracing_subscriber::{filter::EnvFilter, filter::LevelFilter, fmt, prelude::*};
 
 use codecrafters_redis::handler::handle;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+
+    // Set up logging
+    let format = fmt::format()
+        .without_time()
+        .with_level(true) // include debug level in formatted output
+        .with_target(true) // include targets (module)
+        .with_ansi(false) // no pretty colours
+        .compact(); // use the `Compact` formatting style.
+    let format_layer = tracing_subscriber::fmt::layer()
+        .with_writer(io::stderr)
+        .event_format(format);
+    let filter = EnvFilter::from_default_env().add_directive(LevelFilter::INFO.into());
+    tracing_subscriber::registry()
+        .with(format_layer)
+        .with(filter)
+        .init();
+    
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
-    println!("Listening {listener:?}");
+    info!("Listening on {listener:?}");
     
     loop {
         let (stream, _) = listener.accept().await?;
         let mut stream = BufStream::new(stream);
 
         tokio::spawn(async move {
-            let r = handle(&mut stream).await;
-            println!("{r:?}");
+            handle(&mut stream).await.unwrap()
         });
     }
 }
